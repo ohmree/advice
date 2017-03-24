14f0558df49eb08dbf373ea8654627c788bc1bdd
omrisim210 <omrisim@gmail.com>
2017-03-24 21:04:06 +0300

    Make it loop indefinitely
    Currently only quittable by pressing the 'X' button because (read-char) does buffering

--- src/advice.lisp
+++ src/advice.lisp
@@ -1,4 +1,10 @@
+(ql:quickload :jonathan)
+(ql:quickload :dexador)
 (defun main ()
-  (ql:quickload :jonathan)
-  (ql:quickload :dexador)
-  (princ (first (last (first (last (jonathan:parse (dex:get "http://api.adviceslip.com/advice"))))))) (read-char))
+  (format t "Press enter to show an advice~%~%")
+  (read-char)
+  (loop
+    (format t "~A~%~%"
+     (first (last (first (last
+     (jonathan:parse (dex:get "http://api.adviceslip.com/advice")))))))
+     (read-char)))
